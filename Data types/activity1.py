6f01c776e4c8c4afb00e166f6ba9b40298328ca8
Idhantsaud <idhusaud@gmail.com>
2024-08-29 21:40:43 +0545

    activity1 of data types

---
+++ Data types/activity1.py
@@ -0,0 +1,8 @@
+a = 5
+b = 3.5
+d = 'Idhant'
+e = False
+print("Data type of a = ", type(a))
+print("Data type of b = ", type(b))
+print("Data type of d = ", type(d))
+print("Data type of e = ", type(e))
