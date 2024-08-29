6d2d4b7e289caae51961d047044b75b11b34fc26
Idhantsaud <idhusaud@gmail.com>
2024-08-29 21:48:52 +0545

    activity 2 data type

---
+++ Data types/activity2.py
@@ -0,0 +1,10 @@
+a = 5
+b = 2.5
+print("Data type of a = ", type(a))
+print("Data type of b = ", type(b))
+a = str(a)
+b = int(b)
+print("Data type of a after conversion = ", type(a))
+print("Data type of b after conversion = ", type(b))
+print(a)
+print(b)
