772779af44b326af778ae7f4d51b4dbad7fbbbf6
Idhantsaud <idhusaud@gmail.com>
2024-09-05 21:52:15 +0545

    task3

---
+++ Operators pt1/task3.py
@@ -0,0 +1,8 @@
+a = int(input("Enter marks for subject 1: "))
+b = int(input("Enter marks for subject 2: "))
+c = int(input("Enter marks for subject 3: "))
+d = int(input("Enter marks for subject 4: "))
+
+percentage = ((a+b+c+d)/400)*100
+print("Your percentage is: ", percentage)
+
