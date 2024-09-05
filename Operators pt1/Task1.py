468cd15f86f276abd659f70a83e6212da5518362
Idhantsaud <idhusaud@gmail.com>
2024-09-05 21:45:24 +0545

    task1

---
+++ Operators pt1/Task1.py
@@ -0,0 +1,8 @@
+a = 50
+b = 30
+c = 10
+d = 70
+
+average = (a+b+c+d)/4
+print("This is the average", average)
+
---
+++ Operators pt1/task2.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the amount"))
+note_100 = a//100
+note_50 = (a%100)//50
+note_10 = ((a%100)%50)//10
+
+print("Number of 100 rupees are: ", note_100)
+print("Number of 50 rupees are: ", note_50)
+print("Number of 10 rupees are: ", note_10)
+
