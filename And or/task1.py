ab2601218046429c607f678cce97b39c1601f1a0
Idhantsaud <idhusaud@gmail.com>
2024-09-12 21:12:49 +0545

    task1

---
+++ And or/task1.py
@@ -0,0 +1,22 @@
+a = 3
+b = 0
+c = 2
+
+if a and b and c:
+    print("All the numbers have boolean value as True")
+else:
+    print("At least one of the numbers has the boolean value as False")
+
+a = 5
+b = -8
+c = 0
+
+if a > 0 or b > 0:
+    print("At least one value is greater than 0")
+else:
+    print("None of the values are greater than 0")
+
+if b > 0 or c > 0:
+    print("At least one value is greater than 0")
+else:
+    print("None of the values are greater than 0")
--- IF statments/Activity2.py
+++ IF statments/Activity2.py
@@ -1,12 +1,14 @@
-actual_cost = int(input("Enter the actual cost: "))
-selling_price = int(input("Enter the selling price: "))
+actual_cost = int(input("Enter the actual cost to make the product: "))
+selling_price = int(input("Enter the selling price for the product: "))
 
 if (selling_price>actual_cost):
      profit = selling_price-actual_cost
      print("The profit you made is: ", profit)
 elif (selling_price == actual_cost):
      print("You did not lose any money but you also didn't make any profit")
-else:
-     print("You lost money")
+elif (selling_price<actual_cost):
+     loss = selling_price-actual_cost
+     print("You lost:", loss)
+     
 
 
