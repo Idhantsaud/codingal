57ba7da4719342a119d15010f326ed351ef31ae0
Idhantsaud <idhusaud@gmail.com>
2024-09-12 21:31:53 +0545

    task

---
+++ And or/task2.py
@@ -0,0 +1,21 @@
+a = 6
+b = 6
+c = 3
+
+print(a != b)
+print(b != c)
+a = "Python"
+b = "Coding"
+if a != b :
+    print("a and b are different")
+else:
+    print("a and b are the same")
+
+a = 8
+b = 9
+c = 8
+if (a == 1)!=(b == 9):
+    print("Hello")
+a = int(input("Enter a number"))
+if a%2 != 0 :
+    print("The number is odd")
