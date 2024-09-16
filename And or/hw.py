0b5fdd3889d1f79f873ff5bf85206b6c45a08d39
Idhantsaud <idhusaud@gmail.com>
2024-09-16 18:39:42 +0545

    hw

---
+++ And or/hw.py
@@ -0,0 +1,11 @@
+temperature = int(input("Enter the temperature in celcius:")) 
+if temperature < 0 > temperature:
+    print("Wear something warm today it's very cold ")
+elif temperature < 5 > temperature:
+    print("It's still cold but you might not need a big jacket")
+elif temperature < 15 > temperature:
+    print("It's not very cold but you might need a light jacket")
+elif temperature > 25:
+    print("Very warm today wear something light")
+else:
+    print("Go for a swim it's too hot today")
